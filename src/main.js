77d4599bcede8d9b42dcb81b0830ebfc4c0334a3
rubialburqueque <rubi.vap@gmail.com>
2019-12-13 15:19:51 -0500

    actiliza con spa
    actualizé toda la data

---
+++ src/main.js
@@ -0,0 +1,32 @@
+import { changeTmp } from './view-controller/router.js';
+import { correctRegiste } from './function.js';
+
+const init = () => {
+  changeTmp(window.location.hash);
+  window.addEventListener('hashchange', () => changeTmp(window.location.hash));
+};
+window.addEventListener('load', init);
+
+const elementsRegister = formRegister.elements;
+const regex = /\S+@\S+\.\S+/;
+console.log(elementsRegister);
+console.log(elementsRegister.name.value);
+
+document.getElementById('formRegister').addEventListener('submit', (event) => {
+  event.preventDefault();
+  console.log('ingresa al button');
+  console.log(regex.test(elementsRegister.email.value));
+
+  if (elementsRegister.password.value.length >= 6 && elementsRegister.password.value === elementsRegister.confirmPassword.value) {
+    if (regex.test(elementsRegister.email.value) === true) {
+      correctRegiste(elementsRegister.name.value,
+        elementsRegister.lastName.value,
+        elementsRegister.email.value,
+        elementsRegister.brithday.value,
+        elementsRegister.password.value,
+        elementsRegister.confirmPassword.value);
+    }
+  } else {
+    alert('Las contraseñas no son iguales');
+  }
+});
---
+++ src/view/index.js
@@ -0,0 +1,17 @@
+import Home from './home.js';
+import Register from './register.js';
+import Foro from './foro.js';
+import Contactenos from './contacto.js';
+import Different from './404.js';
+
+
+const components = {
+  home: Home,
+  register: Register,
+  foro: Foro,
+  contacto: Contactenos,
+  different: Different,
+};
+
+
+export { components };
