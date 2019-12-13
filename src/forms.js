ac064f9ef097bf15fc9ea05bd7e892c74efecd4d
naty4393 <naty4393@gmail.com>
2019-12-13 15:45:50 -0500

    update modificate and add forms rex

--- src/forms.js
+++ src/forms.js
@@ -1,16 +1,25 @@
 import { correctRegiste } from './function.js';
 
 const elementsRegister = formRegister.elements;
+const regex = /\S+@\S+\.\S+/;
 console.log(elementsRegister);
 console.log(elementsRegister.name.value);
 
 document.getElementById('formRegister').addEventListener('submit', (event) => {
     event.preventDefault();
     console.log('ingresa al button');
-    correctRegiste(elementsRegister.name.value,
-        elementsRegister.lastName.value,
-        elementsRegister.email.value,
-        elementsRegister.brithday.value,
-        elementsRegister.password.value,
-        elementsRegister.confirmPassword.value);
+    console.log(regex.test(elementsRegister.email.value));
+
+    if (elementsRegister.password.value.length >= 6 && elementsRegister.password.value === elementsRegister.confirmPassword.value) {
+        if (regex.test(elementsRegister.email.value) === true) {
+            correctRegiste(elementsRegister.name.value,
+                elementsRegister.lastName.value,
+                elementsRegister.email.value,
+                elementsRegister.brithday.value,
+                elementsRegister.password.value,
+                elementsRegister.confirmPassword.value);
+        }
+    } else {
+        alert('Las contraseñas no son iguales')
+    }
 })
