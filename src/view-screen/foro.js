c2c8d1bfbfbb88d8784d2fec58f24f24258537c1
liliananolasco <liliana.nolasco@gmail.com>
2019-12-16 18:57:54 -0500

    empezando con los poss

--- src/view-screen/foro.js
+++ src/view-screen/foro.js
@@ -1,24 +1,33 @@
 export default () => {
   const viewForo = `
   <section id="foro" ><p class="titulo">girl ASK</p>
-  <form id="foro">
-  <div id = "perfil">
-  <img src = "../imagen/foto.jpeg" class = "foto" />
-  <p id = "nombre"></p>
-  </div>
+    <div id = "perfil">
+      <img src = "../imagen/foto.jpeg" class = "foto" /><p id = "nombre"></p>
+    </div>
     </br>
-    <input id="comentario" type="text" placeholder="¿Que quieres compartir?....">
+    <input id="comit" type="text" placeholder="¿Que quieres compartir?....">
     </br>
-    <button type="button" id="compartir">compartir</button>
-  </form>
-  <p>O bien ingresa con ...</p>
+    <button type="button" id="share">compartir</button>
+    <div>
+    <section id = "comits"> 
+      <p id = "name-user"></p>
+      <p id = "fecha"></p>
     </section>
-  <section><img src = "img/computadora.png" alt="" class = "img-principal"/></section>`;
+    </div>
+  </section>`;
 
   const divElemt = document.createElement('div');
   divElemt.classList.add('position');
   divElemt.innerHTML = viewForo;
 
   divElemt.querySelector('#nombre').innerHTML = localStorage.getItem('nombreUsuario');
+  let text = [];
+  const btnCompartir = divElemt.querySelector('#share');
+  btnCompartir.addEventListener('click', () => {
+    text += divElemt.querySelector('#comit').value;
+    divElemt.querySelector('#comits').innerHTML = text;
+    divElemt.querySelector('#name-user').innerHTML = localStorage.getItem('nombreUsuario');
+  });
+
   return divElemt;
 };
