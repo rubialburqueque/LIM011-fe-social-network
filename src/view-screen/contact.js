e6376fa1ce7b18cde56d81333d510ca3c033e8dc
liliananolasco <liliana.nolasco@gmail.com>
2019-12-20 01:22:43 -0500

    pintado de posts

--- src/view-screen/contact.js
+++ src/view-screen/contact.js
@@ -7,10 +7,6 @@ export const showComentarios = (data) => {
     <p>${prueba.nombre}</p>
     <p>${prueba.texto}</p>
     </section>`;
-    viewContact = document.createElement('div');
-    /* divElemt.querySelector('#commit-publicado').innerHTML = data[6].texto;
-    divElemt.querySelector('#name-user').innerHTML = data[].nombre;
-    console.log(data[0].texto); */
-    return viewContact;
   });
+  return viewContact;
 };
