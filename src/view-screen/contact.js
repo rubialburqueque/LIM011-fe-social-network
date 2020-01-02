dec8ee849ca5e4e5e566a19d5a8b4e20b450e93f
liliananolasco <liliana.nolasco@gmail.com>
2020-01-02 14:30:02 -0500

    comentario

--- src/view-screen/contact.js
+++ src/view-screen/contact.js
@@ -11,10 +11,17 @@ export const showComentarios = (data) => {
       <section class="comment-head">
         <h6 class="comment-name by-author">${comentario.nombre}</h6>
         <span>hace 20 minutos</span>
+        <p>eliminar</p>
         <i class="fa fa-reply"></i>
         <i class="fa fa-heart"></i>
       </section>
       <section class="comment-content">${comentario.texto}</section>
+      <section class="comment-opinion">
+        <p>Me gusta</p> <br>
+        <p>Comentar</p>
+        <i class="fa fa-reply"></i>
+        <i class="fa fa-heart"></i>
+      </section>
     </div>
   </div> `;
   });
