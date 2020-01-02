fd8523bf5c9c5bd5e1c75458d3bd2bd5e360d098
rubialburqueque <rubi.vap@gmail.com>
2020-01-02 14:27:03 -0500

    actualizacion post vacaciones
    post vacaciones

--- src/view-screen/index.js
+++ src/view-screen/index.js
@@ -1,12 +1,16 @@
-import Home from './home.js';
+import Login from './login.js';
 import Register from './register.js';
 import Foro from './foro.js';
+import ModifyProfile from './modifyProfile.js';
+// import Contact from './contact.js';
 import Different from './404.js';
 
 
 export const components = {
-  home: Home,
+  login: Login,
   register: Register,
   foro: Foro,
+  modifyProfile: ModifyProfile,
+  // contact:Contact
   different: Different,
 };
