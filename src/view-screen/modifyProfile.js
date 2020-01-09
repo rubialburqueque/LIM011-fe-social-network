b28ab254d27b228d6e0a52883e95304ccf24b15a
rubialburqueque <rubi.vap@gmail.com>
2020-01-08 19:08:46 -0500

    preparar function para testear
    preparar function para testear

--- src/view-screen/modifyProfile.js
+++ src/view-screen/modifyProfile.js
@@ -2,19 +2,19 @@ export default () => {
   const viewModifyProfile = `
     <div class="container-register">
     <form id="formRegister">
-        <label for="name" class="label">Nombre:</label></br>
-        <input type="text" id="name" class="inputs-register"></br>
-        <label for="lastNames" class="label">Apellidos:</label></br>
-        <input type="text" id="lastName" class="inputs-register"></br>
-        <label for="brithday" class="label">Cumpleaños</label></br>
-        <input type="text" id="brithday" placeholder="01-01-01" class="inputs-register"></br>
-        <label for="email" class="label">Email:</label></br>
-        <input type="email" id="email" class="inputs-register"></br>
-        <label for="password" class="label">password:</label></br>
-        <input type="password" id="password" class="inputs-register"></br>
-        <label for="confirmPassword" class="label">Confirm password:</label></br>
-        <input type="password" id="confirmPassword" class="inputs-register"></br>
-        <input type="submit" id="buttonModifyProfile" class="button" value="modificar">
+      <label for="name" class="label">Nombre:</label></br>
+      <input type="text" id="name" class="inputs-register"></br>
+      <label for="lastNames" class="label">Apellidos:</label></br>
+      <input type="text" id="lastName" class="inputs-register"></br>
+      <label for="brithday" class="label">Cumpleaños</label></br>
+      <input type="text" id="brithday" placeholder="01-01-01" class="inputs-register"></br>
+      <label for="email" class="label">Email:</label></br>
+      <input type="email" id="email" class="inputs-register"></br>
+      <label for="password" class="label">password:</label></br>
+      <input type="password" id="password" class="inputs-register"></br>
+      <label for="confirmPassword" class="label">Confirm password:</label></br>
+      <input type="password" id="confirmPassword" class="inputs-register"></br>
+      <input type="submit" id="buttonModifyProfile" class="button" value="modificar">
     </form>
 </div>`;
 
--- test/example.spec.js
+++ test/example.spec.js
@@ -2,6 +2,10 @@ import { correctRegister, guardarPost } from '../src/function.js';
 
 describe('correctRegister', () => {
   it('debería ser una función', () => {
-    expect(typeof example).toBe('function');
+    expect(typeof correctRegister).toBe('function');
   });
+  it('deberia de guardar la data del usuario', () => {
+    expect(localStorage.getItem('registro')).toBe(undefined)
+    correctRegister()
+  })
 });
